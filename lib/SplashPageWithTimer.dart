9c487f2d3594d4353d9b03e138587f77c797ac3d
tuozhaobing <1044176601@qq.com>
2019-04-17 15:43:04 +0800

    SplashPage with timer completed

--- lib/SplashPageWithTimer.dart
+++ lib/SplashPageWithTimer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class SplashPageWithTimer extends StatefulWidget {
   @override
@@ -8,6 +9,48 @@ class SplashPageWithTimer extends StatefulWidget {
 }
 
 class _SplashPageWithTimerState extends State<SplashPageWithTimer> {
+
+  int timeTotal = 3;
+  int countTime = 0;
+
+  Timer timer;
+
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  @override
+  void dispose() {
+    releaseTimer();
+    super.dispose();
+  }
+
+  void startTimer(){
+    timer = new Timer.periodic(const Duration(seconds: 1), (timer){
+      if (timeTotal - countTime > 0){
+        setState(() {
+          countTime++;
+          timeTotal--;
+        });
+      }else{
+        goHomePage();
+      }
+    });
+  }
+
+  void releaseTimer(){
+    countTime = 0;
+    timeTotal = 3;
+    timer.cancel();
+    timer = null;
+  }
+
+  void goHomePage(){
+    Navigator.of(context).pushReplacementNamed('homePage');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -27,22 +70,36 @@ class _SplashPageWithTimerState extends State<SplashPageWithTimer> {
           child: new Text(
             "Helio",
             style: new TextStyle(
-              fontFamily: 'DancingBold',
               decoration: TextDecoration.none,
               color: Colors.white,
               fontSize: 60,
             ),
           ),
         ),
-        new Text(
-          "5",
-          style: new TextStyle(
-            fontFamily: 'DancingBold',
-            decoration: TextDecoration.none,
-            color: Colors.white,
-            fontSize: 60,
+        new Container(
+          constraints: new BoxConstraints.expand(
+            width: 30,
+            height: 30
           ),
-        ),
+          decoration: new BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.white,
+            border: new Border.all(
+              color: Colors.lightBlueAccent,
+              width: 2.5
+            )
+          ),
+          alignment: Alignment.center,
+          margin: EdgeInsets.only(left: 360,top: 40),
+          child: new Text(
+            '$timeTotal',
+            style: new TextStyle(
+              decoration: TextDecoration.none,
+              color: Colors.black54,
+              fontSize: 13,
+            ),
+          ),
+        )
       ],
     );
   }
