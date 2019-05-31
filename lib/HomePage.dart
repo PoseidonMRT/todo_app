19a173d6d8c51d6cf233d9ff2f8c8ce4419bce9e
tuozhaobing <1044176601@qq.com>
2019-05-31 16:12:46 +0800

    add title in home app bar

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -10,7 +10,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
+        title: Text('MainPage'),
       ),
       body: Center(
         child: Column(
