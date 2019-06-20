e100016de26dba425f9593b63415dc49f6ed2d78
tuozhaobing <1044176601@qq.com>
2019-06-20 22:58:01 +0800

    user guide page completed navigation fix to SwipeHomePage

--- lib/bottompage/SettingsPage.dart
+++ lib/bottompage/SettingsPage.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class SettingsPage extends StatefulWidget {
-
   @override
   _SettingsPageState createState() {
     return new _SettingsPageState();
@@ -9,9 +8,24 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-
   @override
   Widget build(BuildContext context) {
-    return Text("PersonPage");
+    return new Container(
+      color: Colors.white,
+      child: ListView(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 10, left: 15, bottom: 10),
+            child: new Text(
+              "General",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 22),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
